5d74a4cb6aa65c74c678e04ddf959e93977664cb
Pratik Keluskar <65060276+Pratik-1997@users.noreply.github.com>
2023-04-08 14:56:54 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,11 +4,18 @@ $(document).ready(function(){
     AOS.init();
   
     // Popup
+    popup = $(".pop_up_block");
+    if(popup.hasClass("show")){
+      $("body").css("overflow" , "hidden");
+    }
+
     $(".pop_up_block .pop_up_container .close_btn").click(function(){
       $(".pop_up_block").hide();
+      $(popup.removeClass("show"));
+      $("body").css("overflow" , "auto");
+
     })
     
-
     // Testimonial Slider
     $('.testimonial_slider').slick({
       infinite: true,
@@ -16,7 +23,6 @@ $(document).ready(function(){
       slidesToScroll: 1,
       autoplay: false,
     });
-
 
     // More Less button
     $('.moreless_button').click(function() {
@@ -29,5 +35,3 @@ $(document).ready(function(){
     });
 
   });
-
-  
