0f7570aab33aa140c9cde70821a908e03eb5dce7
Pratik Keluskar <65060276+Pratik-1997@users.noreply.github.com>
2023-04-10 18:36:18 +0530

    Changes

--- js/main.js
+++ js/main.js
@@ -24,6 +24,22 @@ $(document).ready(function(){
       autoplay: false,
     });
 
+    $('.specialization_container').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+    });
+
     // More Less button
     $('.moreless_button').click(function() {
       $('.more_text').slideToggle();
